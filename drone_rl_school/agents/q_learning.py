made sure that both the global learning rate decay and the learning rate per state action pair decay can be run. fixed the formula for the per s-a-p decay.

drone_rl_school/agents/q_learning.py
@@ -4,7 +4,8 @@ class QLearningAgent:
     def __init__(self, vel_bins=4, delta_bins=12, 
                  alpha=0.9, epsilon=1.0, gamma=0.99,
                  alpha_min=0.01, epsilon_min=0.05,
-                 alpha_decay=0.9995, epsilon_decay=0.9998):
+                 alpha_decay=0.3, epsilon_decay=0.9999,
+                 alpha_per_state=True):
         # The number of bins per dimension
         self.vel_bins = vel_bins
         self.delta_bins = delta_bins
@@ -14,10 +15,13 @@ class QLearningAgent:
 
         # Learning hyperparameters
         self.alpha_0 = alpha
-        # self.alpha = np.full((self.vel_bins, self.vel_bins, self.vel_bins, 
-        #                          self.delta_bins, self.delta_bins, self.delta_bins, 
-        #                          self.num_actions), alpha)  # learning rate for each state
-        self.alpha = alpha
+        self.alpha_per_state = alpha_per_state
+        if self.alpha_per_state:
+            self.alpha = np.full((self.vel_bins, self.vel_bins, self.vel_bins, 
+                                    self.delta_bins, self.delta_bins, self.delta_bins, 
+                                    self.num_actions), alpha)  # learning rate for each state action pair
+        else:
+            self.alpha = alpha    # one global learning rate
         self.gamma = gamma  # discount factor
         self.epsilon_0 = epsilon  # exploration factor
         self.epsilon = epsilon
@@ -40,8 +44,8 @@ class QLearningAgent:
         self.epsilon = max(self.epsilon_min, self.epsilon_0 * self.epsilon_decay ** episodes)
 
 
-    def decay_alpha(self):
-        adapted_decay_rate = np.power(self.alpha_decay, self.visit_count / np.sum(self.visit_count))
+    def decay_individual_alpha(self):
+        adapted_decay_rate = np.power(self.alpha_0, self.visit_count)
         self.alpha = np.maximum(self.alpha_min, self.alpha_0 * adapted_decay_rate)
 
 
@@ -74,14 +78,12 @@ class QLearningAgent:
         max_next_q = np.max(self.q_table[*discretized_next_state])
 
         # Q-learning update rule
-        # self.q_table[*discretized_state, action] = \
-        #     current_q + self.alpha[*discretized_state, action] * (reward + self.gamma * max_next_q - current_q)
-        self.q_table[*discretized_state, action] = \
-            current_q + self.alpha * (reward + self.gamma * max_next_q - current_q)
+        if self.alpha_per_state:
+            self.q_table[*discretized_state, action] = \
+                current_q + self.alpha[*discretized_state, action] * (reward + self.gamma * max_next_q - current_q)
+        else:
+            self.q_table[*discretized_state, action] = \
+                current_q + self.alpha * (reward + self.gamma * max_next_q - current_q)
 
-        # Count the visit
+        # Count the visits per state action pair
         self.visit_count[*discretized_state, action] += 1
-
-        # Decay alpha after each visit
-        if alpha_individual_decay:
-            self.alpha[*discretized_state, action] *= self.alpha_decay

drone_rl_school/notebooks/q_learning.py
@@ -22,18 +22,24 @@ def train(agent, env, episodes, epsilon_decay, alpha_global_decay, alpha_individ
                 env.render()
         rewards.append(ep_reward)
 
-        # Log values
-        writer.add_scalar('reward', ep_reward, episode)
-        writer.add_scalar('alpha_median', np.median(agent.alpha), episode)
-        writer.add_scalar('alpha_mean', np.mean(agent.alpha), episode)
-        writer.add_scalar('epsilon', agent.epsilon, episode)
-
+        # Run the requested per episode decays
         if epsilon_decay:
             agent.decay_epsilon(episode)
         if alpha_global_decay:
             agent.decay_global_alpha(episode)
+        if alpha_individual_decay:
+            agent.decay_individual_alpha()
 
-        # Print on training overview
+        # Log values
+        writer.add_scalar('reward', ep_reward, episode)
+        writer.add_scalar('alpha_median', np.median(agent.alpha), episode)
+        writer.add_scalar('alpha_mean', np.mean(agent.alpha), episode)
+        writer.add_scalar('alpha_min', np.min(agent.alpha), episode)
+        writer.add_scalar('alpha_max', np.max(agent.alpha), episode)
+        writer.add_scalar('alpha_sum', np.sum(agent.alpha), episode)
+        writer.add_scalar('epsilon', agent.epsilon, episode)
+
+        # Print a training overview
         if (episode+1) % 200 == 0:
             print(f"Episode {episode+1-200} to {episode+1} \tMedian Reward: {np.median(rewards[-200:]):.2f}")
 
@@ -74,8 +80,8 @@ if __name__ == '__main__':
         episodes = 10_000
         
         epsilon_decay = True
-        alpha_global_decay = True
-        alpha_individual_decay = False
+        alpha_global_decay = False
+        alpha_individual_decay = True
 
         ep_count, rewards = train(agent, env, episodes,
                         epsilon_decay, alpha_global_decay, alpha_individual_decay, 