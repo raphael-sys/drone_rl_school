98bd19cbbee82a786f04c203e66d84b314192404
Raphael Zehner <raphael.zehner@samsongroup.com>
2025-07-03 13:48:59 +0200

    implemented a jupyter notebook for faster iterating with agents and simulations.

--- drone_rl_school/envs/point_mass_env.py
+++ drone_rl_school/envs/point_mass_env.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
-from gymnasium import spaces
+from IPython.display import display
+from ipywidgets import Play, IntSlider, jslink, VBox
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
@@ -115,25 +116,133 @@ class PointMassEnv(gym.Env):
 
         return self._get_obs(), reward, done, {}
 
-    def simulate(self, agent, episodes=1):
+    def simulate_and_plot(self, agent, episodes=1):
         for ep in range(episodes):
             obs = self.reset()
             done = False
             ep_reward = 0
-            while not done:
-                action = agent.choose_action(obs)
-                obs, r, done, _ = self.step(action)
-                self.render()
-                ep_reward += r
-                # End simulation on 'q' key press
-                if self.stop_animation:
-                    self.stop_animation = False
-                    break
+            try:
+                while not done:
+                    action = agent.choose_action(obs)
+                    obs, r, done, _ = self.step(action)
+                    self.render()
+                    ep_reward += r
+                    # End simulation on 'q' key press
+                    if self.stop_animation:
+                        self.stop_animation = False
+                        break
+            except Exception as e:
+                print("Simulation interrupted: ", e)
             plt.close(self.fig)
             self.fig = None
             self.ax = None
             plt.ioff()
             print(f'Simulated Episode    Total Reward: {ep_reward}')
+
+    def simulate(self, agent, episodes=1):
+        trajectory = []
+        obs = self.reset()
+        done = False
+        ep_reward = 0
+        try:
+            while not done:
+                action = agent.choose_action(obs)
+                obs, r, done, _ = self.step(action)
+                ep_reward += r
+                trajectory.append(self.pos.copy())
+                # End simulation on 'q' key press
+                if self.stop_animation:
+                    self.stop_animation = False
+                    break
+        except Exception as e:
+            print("Simulation interrupted: ", e)
+        print(f'Simulated Episode    Total Reward: {ep_reward}')
+        return trajectory
+
+    def animate(self, trajectory, goal):
+        max_frames = len(trajectory)
+
+        # Set up the figure
+        fig = plt.figure(figsize=(12, 8))
+        fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+        
+        # Define grid: 3 rows × 4 columns
+        gs = gridspec.GridSpec(3, 4, width_ratios=[3, 1, 1, 1], height_ratios=[1, 1, 1])
+
+        # 3D view
+        ax3d = fig.add_subplot(gs[:, :3], projection='3d')
+        ax3d.set_title('3D View')
+        ax3d.set_xlim(goal[0] - 10, goal[0] + 10)
+        ax3d.set_ylim(goal[1] - 10, goal[1] + 10)
+        ax3d.set_zlim(goal[2] - 10, goal[2] + 10)
+        ax3d.scatter(*goal, color='red', s=100, label='Goal')
+        plot_3d_pos = ax3d.scatter(*trajectory[0], color='blue', s=50, label='Drone')
+        ax3d.legend(loc='upper left')
+        # XY plane
+        ax_xy = fig.add_subplot(gs[0, 3])
+        ax_xy.set_title('XY Projection')
+        ax_xy.set_xlim(goal[0] - 10, goal[0] + 10)
+        ax_xy.set_ylim(goal[1] - 10, goal[1] + 10)
+        ax_xy.scatter(goal[0], goal[1], color='red', s=80)
+        plot_xy_pos = ax_xy.scatter(trajectory[0][0], trajectory[0][1], color='blue', s=40)
+        ax_xy.xaxis.grid(); ax_xy.yaxis.grid()
+        ax_xy.set_xlabel('X'); ax_xy.set_ylabel('Y')
+        # XZ plane
+        ax_xz = fig.add_subplot(gs[1, 3])
+        ax_xz.set_title('XZ Projection')
+        ax_xz.set_xlim(goal[0] - 10, goal[0] + 10)
+        ax_xz.set_ylim(goal[2] - 10, goal[2] + 10)
+        ax_xz.scatter(goal[0], goal[2], color='red', s=80)
+        plot_xz_pos = ax_xz.scatter(trajectory[0][0], trajectory[0][2], color='blue', s=40)
+        ax_xz.xaxis.grid(); ax_xz.yaxis.grid()
+        ax_xz.set_xlabel('X'); ax_xz.set_ylabel('Z')
+        # YZ plane
+        ax_yz = fig.add_subplot(gs[2, 3])
+        ax_yz.set_title('YZ Projection')
+        ax_yz.set_xlim(goal[1] - 10, goal[1] + 10)
+        ax_yz.set_ylim(goal[2] - 10, goal[2] + 10)
+        ax_yz.scatter(goal[1], goal[2], color='red', s=80)
+        plot_yz_pos = ax_yz.scatter(trajectory[0][1], trajectory[0][2], color='blue', s=40)
+        ax_yz.xaxis.grid();ax_yz.yaxis.grid()
+        ax_yz.set_xlabel('Y'); ax_yz.set_ylabel('Z')
+
+        plt.tight_layout()
+        plt.ion()
+
+        # Update the view
+        def update(frame):
+            # 3D axis
+            plot_3d_pos._offsets3d = ([trajectory[frame][0]], [trajectory[frame][1]], [trajectory[frame][2]])
+            dist = np.linalg.norm(goal - trajectory[frame])
+            ax3d.set_title(f'3D  |  Step {frame:03d}  |  Dist {dist:.2f}')
+
+            # XY projection
+            plot_xy_pos.set_offsets([trajectory[frame][0], trajectory[frame][1]])
+
+            # XZ projection
+            plot_xz_pos.set_offsets([trajectory[frame][0], trajectory[frame][2]])
+
+            # YZ projection
+            plot_yz_pos.set_offsets([trajectory[frame][1], trajectory[frame][2]])
+
+        # Buttons
+        play = Play(
+            value=0,
+            min=0,
+            max=max_frames-1,
+            step=1,
+            interval=250,       # milliseconds between frames
+            description="▶️",
+            disabled=False
+        )
+
+        # Link the animation to the play button
+        play.observe(lambda ev: update(ev['new']), names='value')
+
+        # Show the viewer
+        display(VBox([play]))
+        # Trigger initial draw
+        update(0)
 
     def render(self, mode='human'):
         # Initialize plot
@@ -183,6 +292,7 @@ class PointMassEnv(gym.Env):
 
             plt.tight_layout()
             plt.ion()
+            self.fig.show()
 
         # 3D axis
         self.plot_3d_pos._offsets3d = ([self.pos[0]], [self.pos[1]], [self.pos[2]])
@@ -199,5 +309,5 @@ class PointMassEnv(gym.Env):
         self.plot_yz_pos.set_offsets([self.pos[1], self.pos[2]])
 
         # Redraw
-        plt.draw()
+        self.fig.canvas.draw()
         plt.pause(0.01)
