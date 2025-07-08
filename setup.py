be76d6bbe0221c6e33b0877a55da562248bb2913
Raphael Zehner <raphael.zehner@samsongroup.com>
2025-07-08 14:37:41 +0200

    introduced the sources folder in preparation for the pytest set up. added a preliminary pytest ini file.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="drone_rl_school",                
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
